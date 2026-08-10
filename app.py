7a4477ef9023c4ca9b5a5b1a00fc77d131a97c8c
akiferkam16-debug <akiferkam16@gmail.com>
2026-08-10 18:28:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "Erkam_Miknatis_Guvenli_Anahtar_2024") 
 
 # --- Admin Ayarları ---
-ADMIN_PASSWORD =  "turmişijorun" 
+ADMIN_PASSWORD =  "kaplantest" 
 DATA_FILE = "products.json"
 
 # --- Başlangıç Ürün Veri Bankası ---
